6bf135c527915879d2798d49431ff36b0eff01fa
Rostislav <rvssv@bk.ru>
2022-08-16 16:18:19 +0300

    created new branch feature_head

---
+++ src/components/Head.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="head-container">
+        <HeadTitle/>
+        <Select/>
+    </div>
+</template>
+
+<script>
+    import HeadTitle from "@/components/HeadTitle";
+    import Select from "@/components/Select";
+
+    export default {
+        name: "Head",
+        components: {Select, HeadTitle}
+    }
+</script>
+
+<style lang="scss">
+    .head-container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin: 0 0 50px 0;
+    }
+</style>
---
+++ src/components/HeadTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="head-title-wrapper">
+        <h1 class="head-title">Список задач</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeadTitle"
+    }
+</script>
+
+<style lang="scss">
+    .head-title {
+        font-size: $bigTitle;
+        font-weight: $bolderFontWeight;
+        line-height: $bigLineHeight;
+        color: $blackColor;
+    }
+</style>
