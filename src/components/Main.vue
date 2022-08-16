5d7da9fdd639379ace694b35702edd97448e4b63
Rostislav <rvssv@bk.ru>
2022-08-16 16:22:35 +0300

    created new branch feature_main

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main-wrapper">
+        <Head/>
+    </div>
+</template>
+
+<script>
+    import Head from "@/components/Head";
+
+    export default {
+        name: "Main",
+        components: {Head}
+    }
+</script>
+
+<style lang="scss">
+
+</style>
